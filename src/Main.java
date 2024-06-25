59e17ecba0a50ade0f3a80844cb211dd17e4b86d
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-06-25 18:30:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Если человеку " + age +  " лет, то он совершеннолетний");
+        }
+        else {
+            System.out.println("Если человеку " + age +  " лет, то он несовершеннолетний");
+        }
+    }
+}
