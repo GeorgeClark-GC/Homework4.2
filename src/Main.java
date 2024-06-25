c0297fdab06fdbe12fb00194999dd2d99b9c643f
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-06-25 18:43:35 +0300

    Ex2

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,14 @@ public class Main {
         else {
             System.out.println("Если человеку " + age +  " лет, то он несовершеннолетний, нужно ещё немного подождать");
         }
+        int temperature = 2;
+        boolean putHat = temperature >=5;
+        if (putHat)
+        {
+            System.out.println("На улице тепло, " + temperature + " градусов, можно выходить без шапки");
+        }
+        else{
+            System.out.println("На улице холодно, " + temperature + " градусов, нужно выйти в шапке");
+        }
     }
 }
